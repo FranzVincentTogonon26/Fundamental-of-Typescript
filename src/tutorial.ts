655fe4073edf647971ce1130a10c3685cd14f6ab
Jhen2490 <franzvincent@gmail.com>
2026-05-09 16:08:28 +0800

    fundamental of typescript

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -1,3 +1,6 @@
+
+// SOURCE REFERENCE = https://github.com/john-smilga/typescript-course
+
 function processData( 
         input: number | string, 
         config: { reverse: boolean } = { reverse: false } 
@@ -15,7 +18,7 @@ console.log(processData('test'))
 
 
 console.log('----------------------------------------------------------------------------')
-console.log('TYPES')
+console.log('TYPES ALIAS')
 console.log('----------------------------------------------------------------------------')
 
 
@@ -143,4 +146,155 @@ if(isManger(employee)){
 console.log('----------------------------------------------------------------------------')
 
 
+enum UserRole  {
+    Admin = 'admin',
+    Manager = 'manager',
+    Employee = 'employee'
+}
 
+type User = {
+    id: number;
+    name: string;
+    role: UserRole;
+    contact: [ email: string, phone: string ]
+}
+
+function createUser( user: User ) : User {
+    return user
+}
+
+const user : User = {
+    id: 1,
+    name: 'franz',
+    role: UserRole.Admin,
+    contact: [ 'franz@gmail.com', '099834']
+}
+
+console.log(createUser(user))
+
+console.log('----------------------------------------------------------------------------')    
+
+type ValueType = string | number | boolean
+
+let value: ValueType
+
+const random = Math.random()
+value = random < 0.33 ? 'Hello' : random < 0.66 ? 123.456 : true
+
+function checkValue( value: ValueType ) : void {
+    if( typeof value === 'string'){
+        console.log( value.toLowerCase() )
+        return
+    } else if( typeof value === 'number'){
+        console.log( value.toFixed(2))
+        return
+    } else {
+        console.log(`Boolean: ${value}`)
+        return
+    }
+}
+
+checkValue(value)
+
+console.log('----------------------------------------------------------------------------') 
+
+
+type Dog = {
+    type: 'dog';
+    name: string;
+    bark: () => void
+}
+type Cat = {
+    type: 'cat';
+    name: string;
+    meow: () => void
+}
+
+type Animal = Dog | Cat
+
+// function makeSound( animal: Animal ){
+//     if( animal.type === 'dog'){
+//         animal.bark()
+//     } else {
+//         animal.meow()
+//     }
+// }
+
+function makeSound( animal: Animal ){
+    if( 'bark' in animal){
+        animal.bark()
+    } else {
+        animal.meow()
+    }
+}
+
+console.log('----------------------------------------------------------------------------') 
+
+type Student = {
+    name: string;
+    study: () => void
+}
+
+type User1 = {
+    name: string;
+    login: () => void
+}
+
+type Person1 = Student | User1
+
+const randomPerson = () : Person1 => {
+    return Math.random() > 0.5 ? { name: 'franz', study: () => console.log('Studying..')} : { name: 'jen', login: () => console.log('Loging in')}
+}
+
+const person = randomPerson()
+
+function isStudent( person: Person) : person is Student{
+    return ( person as Student ).study !== undefined
+}
+
+if(isStudent(person)){
+    person.study()
+} else {
+    person.login()
+}
+
+console.log('----------------------------------------------------------------------------') 
+
+type IncrementAction = {
+    type: 'increment';
+    amount: number;
+    timestamp: number;
+    user: string;
+}
+type DecrementAction = {
+    type: 'decrement';
+    amount: number;
+    timestamp: number;
+    user: string;
+}
+
+type Action = IncrementAction | DecrementAction
+
+function reducer( state: number, action: Action){
+    switch (action.type){
+        case 'increment':
+            return state + action.amount
+        case 'decrement':
+            return state - action.amount
+        default: {
+            const unexpectedAction: never = action;
+            throw new Error(`Unexpected Action ${unexpectedAction}`)
+        }
+    }
+}
+
+const newState = reducer(5, {
+    type: 'decrement',
+    amount: 2,
+    timestamp: 1213,
+    user: 'franz'
+})
+
+console.log(newState)
+
+console.log('----------------------------------------------------------------------------') 
